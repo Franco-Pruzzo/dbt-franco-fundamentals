273ea6339cae9c8a542a32b7bc823190356e9be7
Franco Pruzzo <franco@dynamicdata.agency>
2026-07-16 19:17:28 +0000

    nuevas carpetas de payments

---
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -0,0 +1,17 @@
+with raw_payments as (
+    select * from `dbt-tutorial.stripe.payment`
+),
+
+renamed as (
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status as payment_status,
+        -- Lo dividimos por 100 para que la cuenta final te dé exacta
+        amount / 100 as payment_amount,
+        created as payment_created
+    from raw_payments
+)
+
+select * from renamed

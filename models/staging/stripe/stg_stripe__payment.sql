28f469ed70726e75bb1d8d369d96f04ed86e5365
Franco-Pruzzo <franco@dynamicdata.agency>
2026-07-17 14:38:52 -0300

    Feat/refactor customers sql query (#1)
    * agregue 2 modelos de staging
    
    * cambie las carpetas y la materializacion de las tablas
    
    * nuevas carpetas de payments
    
    * cree el archivo source.yml
    
    * agregue los freshness
    
    * termine la parte de sources
    
    * cree algunos test

---
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -0,0 +1,17 @@
+with raw_payments as (
+    select * from  {{ source('stripe', 'payment') }}
+),
+
+renamed as (
+    select
+        id as payment_id,
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status as payment_status,
+        -- Lo dividimos por 100 para que la cuenta final te dé exacta
+        amount / 100 as payment_amount,
+        created as created_at
+    from raw_payments
+)
+
+select * from renamed

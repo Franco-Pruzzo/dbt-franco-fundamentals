89d03f432c8c1ac5d2d6c9d22002a213aecd7b2a
Franco Pruzzo <franco@dynamicdata.agency>
2026-08-05 16:55:11 +0000

    jinja y macros

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -8,8 +8,7 @@ renamed as (
         orderid as order_id,
         paymentmethod as payment_method,
         status as payment_status,
-        -- Lo dividimos por 100 para que la cuenta final te dé exacta
-        amount / 100 as payment_amount,
+        {{ cents_to_dollars("amount",2) }} as payment_amount,
         created as created_at,
         _batched_at
         
